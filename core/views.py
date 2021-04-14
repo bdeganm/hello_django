205aeea86dd620f7f2a6a0a71ae6dce299bab668
Bruno Degan <brunodegan@yahoo.com.br>
2021-04-14 19:50:52 -0300

    Inclusão de código referente ao exercício proposto na aula 1

--- core/views.py
+++ core/views.py
@@ -4,3 +4,20 @@ from django.shortcuts import render, HttpResponse
 
 def hello(request, nome, idade):
     return HttpResponse('<h1>Hello {} de {} anos.</h1>'.format(nome, idade))
+
+def soma(request, num1, num2):
+    resultado = num1 + num2
+    return HttpResponse('A soma de {} e {} é igual a {}'.format(num1, num2, resultado))
+
+def subtracao(request, num1, num2):
+    resultado = num1 - num2
+    return HttpResponse('A subtração de {} por {} é igual a {}'.format(num1, num2, resultado))
+
+def multiplicacao(request, num1, num2):
+    resultado = num1 * num2
+    return HttpResponse('O produto de {} e {} é igual a {}'.format(num1, num2, resultado))
+
+
+def divisao(request, num1, num2):
+    resultado = num1 / num2
+    return HttpResponse('A divisão de {} por {} é igual a {}'.format(num1, num2, resultado))
